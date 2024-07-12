0c2d0dc89c9ad7778c00e6c5b01db14a33b5033d
Amirul lutfi <amirullutfi61@gmail.com>
2024-07-12 20:17:46 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus pariatur nihil est qui dolore delectus nostrum laudantium eaque deleniti ipsa iusto veniam, consequuntur quidem in minus sunt accusantium aspernatur aperiam, necessitatibus quia odit voluptates suscipit voluptatibus? Perferendis nesciunt delectus optio asperiores aperiam voluptate exercitationem quod nisi animi nobis maxime dolores consequatur incidunt aliquid, voluptates ratione facilis quo rerum rem nam dolorem in explicabo quis excepturi? Deleniti quis tempore saepe aspernatur vitae rerum, deserunt omnis fugiat recusandae laboriosam perspiciatis sunt. Aliquid, error illum perferendis culpa nulla a labore tenetur odit placeat non iste eaque ipsum laudantium harum, vero reiciendis ipsa. Id!</p>
+    <ul>
+        <li>oke</li>
+    </ul>
 </body>
 </html>
